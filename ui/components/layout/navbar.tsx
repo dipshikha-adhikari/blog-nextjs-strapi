1b83af914d1f8c23afd2e86c0842b888d3e81e08
akbiv <dipshikhaadhikari96@gmail.com>
2024-05-04 23:41:14 +0545

    Husky test pass

--- ui/components/layout/navbar.tsx
+++ ui/components/layout/navbar.tsx
@@ -2,14 +2,11 @@
 import { useAuth } from "@/hooks/use-auth";
 import Link from "next/link";
 import React from "react";
-import { md } from "@/lib/markdown";
 
 const Navbar = () => {
   const { user, isLoading } = useAuth();
   console.log(user);
- 
- 
- 
+
   return (
     <div className="flex justify-between items-center p-10 bg-green-600 text-white">
       <Link href={"/"}>BMDB</Link>
